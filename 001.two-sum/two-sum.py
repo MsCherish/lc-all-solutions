6ab4909c0c6993048e0df2f70e559da238fcc169
MsCherish <37290326+MsCherish@users.noreply.github.com>
2018-04-18 15:05:49 +0800

    Update two-sum.py

--- 001.two-sum/two-sum.py
+++ 001.two-sum/two-sum.py
@@ -5,9 +5,9 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        d = {}
-        for i, num in enumerate(nums):
-            if target - num in d:
+        d = {}                         #用于存储｛值：对应序列｝
+        for i, num in enumerate(nums):  #遍历数组中的索引和对应的元素值
+            if target - num in d:        #直接在d中看其补值在不在d中，如果在直接返回补值的索引和当前考察的索引i
                 return [d[target - num], i]
             d[num] = i
         # no special case handling becasue it's assumed that it has only one solution
