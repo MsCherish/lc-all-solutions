2f0087f44ef11d238d74446119096d76ed52ada6
MsCherish <37290326+MsCherish@users.noreply.github.com>
2018-04-30 19:43:30 +0800

    Update intersection-of-two-arrays.py

--- 349.intersection-of-two-arrays/intersection-of-two-arrays.py
+++ 349.intersection-of-two-arrays/intersection-of-two-arrays.py
@@ -7,11 +7,16 @@ class Solution(object):
         """
         d = {}
         ans = []
-        for num in nums1:
-            d[num] = d.get(num, 0) + 1
+        for num in nums1:       #用hash table保存下数组1的出现的元素
+            d[num] = d.get(num, 0) + 1  #值记录元素出现的次数
         
         for num in nums2:
             if num in d:
                 ans.append(num)
                 del d[num]
         return ans
+    
+    
+         """可以使用hash table保存下数组1的出现的元素，然后判断数组2中的各元素是否在数组1中出现过，但直接使用set更简单"""
+        nums1 = set(nums1)
+        return [x for x in set(nums2) if x in nums1] 
