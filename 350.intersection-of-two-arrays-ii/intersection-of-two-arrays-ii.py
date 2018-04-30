2432b71a4b85ea5b961814da29a7ba6d5dc23e11
MsCherish <37290326+MsCherish@users.noreply.github.com>
2018-04-30 20:44:28 +0800

    Update intersection-of-two-arrays-ii.py

--- 350.intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
+++ 350.intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
@@ -20,3 +20,33 @@ class Solution(object):
                 j += 1
             
         return ans
+    
+    
+    
+        """使用两个字典记录下两个数组中元素出现的次数"""
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        dic1,dic2 = dict(),dict()
+        for num in nums1:
+            dic1[num] = dic1.get(num,0) + 1
+        for num in nums2:
+            dic2[num] = dic2.get(num,0) + 1
+        return [x for x in dic2 for j in range(min(dic1.get(x,0),dic2.get(x,0)))]
+
+"""但是python内置了Counter类型数组，可以方便实现计数功能"""
+import collections
+
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        c1,c2 = collections.Counter(nums1),collections.Counter(nums2)
+        return [i for i in c1.keys() for j in range(min([c1[i], c2[i]]))]
